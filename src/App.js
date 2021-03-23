b5253d4b2b313316d4f8378c9214c25d14d36ca8
Hasham Vakani <hashamvakani@users.noreply.github.com>
2021-03-24 01:02:06 +0500

    feat: add get and add query for library content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+import { useState } from "react";
+const { SiteClient } = require("datocms-client");
+const client = new SiteClient("8205432be590c07a7225d669b4391e");
 
+const GET_AUTHORS = gql`
+  query MyQuery {
+    allAuthors {
+      authorName
+      id
+    }
+  }
+`;
+
+const GET_BOOKS = gql`
+  query MyQuery {
+    allBooks {
+      bookName
+      available
+      authorName {
+        authorName
+        id
+      }
+    }
+  }
+`;
 function App() {
+  const [authorName, setAuthorName] = useState("");
+  const [bookName, setBookName] = useState("");
+  const { error, data, loading } = useQuery(GET_AUTHORS);
+  const { data: dataB } = useQuery(GET_BOOKS);
+
+  async function addAuthor() {
+    const records = await client.items.create({
+      itemType: "616386",
+      author_name: authorName,
+    });
+    console.log(records);
+  }
+
+  async function addBook() {
+    const records = await client.items.create({
+      itemType: "616387",
+      book_name: bookName,
+      available: true,
+      author_name: "25548854",
+    });
+    console.log(records);
+  }
+
+  console.log({ data });
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Add Author</h1>
+        <input type="text" onChange={(e) => setAuthorName(e.target.value)} />
+        <button onClick={addAuthor}>Add Author</button>
+      </div>
+
+      <div>
+        <h1>Add Books</h1>
+        <input type="text" onChange={(e) => setBookName(e.target.value)} />
+        <button onClick={addBook}>Add BookName</button>
+      </div>
+
+      <h1>All Authors</h1>
+      <ul>
+        {data?.allAuthors?.map(({ authorName, id }) => (
+          <li key={id}>{authorName}</li>
+        ))}
+      </ul>
+
+      <h1>All Books</h1>
+      <ul>
+        {dataB?.allBooks?.map(({ bookName, available, authorName : { authorName }, id }) => (
+          <li key={id}>{bookName} | <b>author name:</b> {authorName}</li>
+        ))}
+      </ul>
     </div>
   );
 }
